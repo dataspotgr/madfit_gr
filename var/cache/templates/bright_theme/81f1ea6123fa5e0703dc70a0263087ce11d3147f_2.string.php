bf73aefd70730162db55e5a05ebe2884852cf260
stefkaramichos <stephanoskaramichos@gmail.com>
2025-04-07 13:31:29 +0300

    after merger css_changes

---
+++ var/cache/templates/bright_theme/81f1ea6123fa5e0703dc70a0263087ce11d3147f_2.string.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 4.3.0, created on 2025-04-07 13:30:42
+  from '81f1ea6123fa5e0703dc70a0263087ce11d3147f' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.0',
+  'unifunc' => 'content_67f3a952935823_92533079',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_67f3a952935823_92533079 (Smarty_Internal_Template $_smarty_tpl) {
+?><div class="ty-social-link-block">
+
+
+    <a href="https://www.facebook.com/madfitgr" target="_blank"><img src='/madfit_gr/images/design_images/fb.png' alt="facebook"></a> &nbsp;
+
+
+
+    <a href="https://www.instagram.com/madfit_shop/" target="_blank"><img src='/madfit_gr/images/design_images/insta.png' alt="instagram"></a>
+
+
+
+</div>
+<?php }
+}
